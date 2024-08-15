f124c7b4d950507e51a99826f86164c9ade007ea
WaterHydrant <richard.you.777@gmail.com>
2024-08-15 11:48:16 -0700

    Locking

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import util
+from threading import Lock
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,6 +17,8 @@ user_handle.load_users()
 
 COLOURS = {"Common": discord.Colour.light_gray(), "Rare": discord.Colour.blue(), "Epic": discord.Colour.purple(), "Legendary": discord.Colour.gold()}
 
+users_lock = Lock()
+
 @bot.command()
 async def test(ctx):
     await ctx.send("Test!_")
@@ -24,10 +27,11 @@ async def test(ctx):
     help="Usage: ricky gacha. Performs a wish."
 )
 async def gacha(ctx):
-    user_handle.user_init(ctx.author.id)
-    success, item = user_handle.user_gacha(ctx.author.id)
-
-    user_handle.save_users()
+    with users_lock:
+        user_handle.user_init(ctx.author.id)
+        success, item = user_handle.user_gacha(ctx.author.id)
+        user_handle.save_users()
+        
     if not success:
         time_left = item - datetime.now()
         await ctx.send(ctx.author.mention + ", you must wait **" + util.timeformat(time_left, " days", " hours", " minutes") + "** to wish again!")
@@ -35,14 +39,14 @@ async def gacha(ctx):
         image_file = discord.File("img/"+item["id"]+".png", filename=item["id"]+".png")
         wish_embed = discord.Embed(title=ctx.author.display_name+"'s "+item["name"], description="*"+item["description"]+"*", colour=COLOURS[item["rarity"]]).set_footer(text=item["source"]).set_image(url="attachment://"+item["id"]+".png").set_thumbnail(url=ctx.author.avatar).add_field(name="Rarity", value=item["rarity"]).add_field(name="Collection", value=item["collection"])
                                                                                                                                                                                                                                                                                                                                                                    
-
         await ctx.send(ctx.author.mention + ", wishes left: " + str(user_handle.wishes(ctx.author.id)), file=image_file, embed=wish_embed)
 
 @bot.command(
     help="Usage: ricky progress. Checks collection progress."
 )
 async def progress(ctx):
-    user_handle.user_init(ctx.author.id)
+    with users_lock:
+        user_handle.user_init(ctx.author.id)
 
     info = user_handle.collections(ctx.author.id)
 
@@ -57,10 +61,11 @@ async def progress(ctx):
 )
 async def ability(ctx, *args):
     args = " ".join(args)
-    user_handle.user_init(ctx.author.id)
 
-    ability_info, ability_success = user_handle.ability(ctx.author.id, args)
-    user_handle.save_users()
+    with users_lock:
+        user_handle.user_init(ctx.author.id)
+        ability_info, ability_success = user_handle.ability(ctx.author.id, args)
+        user_handle.save_users()
 
     ability_embed = discord.Embed(title="Ability Used", description=ctx.author.display_name + ", " + ability_info).set_thumbnail(url=ctx.author.avatar).add_field(name="Success", value=ability_success)
     
@@ -71,8 +76,10 @@ async def ability(ctx, *args):
 )
 async def attack(ctx, *args):
     args = " ".join(args)
-    user_handle.user_init(ctx.author.id)
 
+    with users_lock:
+        user_handle.user_init(ctx.author.id)
+        
     guild = ctx.guild
 
     if guild is None:
@@ -85,14 +92,16 @@ async def attack(ctx, *args):
         target = targets[0]
 
     if target is not None:
-        user_handle.user_init(target.id)
+        with users_lock:
+            user_handle.user_init(target.id)
 
         if target.id == ctx.author.id:
             await ctx.send("Can't target yourself with an attack.")
             return
 
-        body, win, loot = user_handle.attack(ctx.author.id, ctx.author.display_name, target.id, target.display_name)
-        user_handle.save_users()
+        with users_lock:
+            body, win, loot = user_handle.attack(ctx.author.id, ctx.author.display_name, target.id, target.display_name)
+            user_handle.save_users()
         
         attack_embed = discord.Embed(title=ctx.author.display_name+"'s onslaught against "+target.display_name, description=body).set_thumbnail(url=ctx.author.avatar).add_field(name="Conclusion", value=win)
         loot_embed = discord.Embed(title="Loot Received", description="*Nice attack. Here's the loot we received from the enemy:*")
@@ -112,11 +121,11 @@ async def attack(ctx, *args):
     help="Usage: ricky items. Lists items owned and status."
 )
 async def items(ctx):
-    user_handle.user_init(ctx.author.id)
-
-    info = user_handle.itemslist(ctx.author.id)
-    stability = user_handle.calculate_stability(ctx.author.id)
-    user_handle.save_users()
+    with users_lock:
+        user_handle.user_init(ctx.author.id)
+        info = user_handle.itemslist(ctx.author.id)
+        stability = user_handle.calculate_stability(ctx.author.id)
+        user_handle.save_users()
     
     items_embed = discord.Embed(title=ctx.author.display_name+"'s Items", description=info).set_thumbnail(url=ctx.author.avatar).add_field(name="Showing", value="Items Owned").add_field(name="Team Stability", value=str(stability*100)+"%")
     await ctx.send(embed=items_embed)
@@ -126,10 +135,11 @@ async def items(ctx):
 )
 async def show(ctx, *args):
     args = " ".join(args)
-    user_handle.user_init(ctx.author.id)
-
-    selected_item = user_handle.select(ctx.author.id, args)
-    user_handle.save_users()
+    
+    with users_lock:
+        user_handle.user_init(ctx.author.id)
+        selected_item = user_handle.select(ctx.author.id, args)
+        user_handle.save_users()
 
     if selected_item == None:
         await ctx.send("Can't show the item: it doesn't exist or you don't own it.")
@@ -146,11 +156,12 @@ async def show(ctx, *args):
     help="Usage: ricky claim. Creates a new land claim (and shows an existing one)"
 )
 async def claim(ctx):
-    user_handle.user_init(ctx.author.id)
-    claim_info = user_handle.create_land_claim(ctx.author.id)
+    with users_lock:
+        user_handle.user_init(ctx.author.id)
+        claim_info = user_handle.create_land_claim(ctx.author.id)
+        user_handle.save_users()
 
     claim_embed = discord.Embed(title=ctx.author.display_name+"'s Land Claim", description=claim_info)
-    user_handle.save_users()
     await ctx.send(embed=claim_embed)
 
     
